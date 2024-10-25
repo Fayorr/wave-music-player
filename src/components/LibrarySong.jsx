781ed6655f8842dff501f60a5c179df50fefe9f0
fayorr <fayokunmiosho@gmail.com>
2024-10-25 19:12:31 +0100

    refactoring

--- src/components/LibrarySong.jsx
+++ src/components/LibrarySong.jsx
@@ -1,23 +1,22 @@
 
 const LibrarySong = ({ isPlaying, audioRef, song, songs, setCurrentSong, id }) => {
 
-const songSelectHandler = () => {
-  setCurrentSong(song);
-  if (isPlaying){
-    const playPromise = audioRef.current.play();
-    if (playPromise !== undefined){
-      playPromise.then((audio) => {
-        audioRef.current.play();
-        
-      })
+  const songSelectHandler = () => {
+    setCurrentSong(song);
+    if (isPlaying) {
+      const playPromise = audioRef.current.play();
+      if (playPromise !== undefined) {
+        playPromise.then((audio) => {
+          audioRef.current.play();
+        })
+      }
     }
+    audioRef.current.play();
   }
-  audioRef.current.play();
-}
 
   return (
     <div onClick={songSelectHandler} className='library-song'>
-       <img src={song.cover} alt="coverPic"></img>
+      <img src={song.cover} alt="coverPic"></img>
       <div className="song-description">
         <h3>{song.name}</h3>
         <h4>{song.artist}</h4>
